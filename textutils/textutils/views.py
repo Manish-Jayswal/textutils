257fff23c51bf4cf58b2af339de2802a0a1f45d2
Manish Jayswal <jayswalmp@gmail.com>
2019-04-15 14:06:39 +0545

    first commit

---
+++ textutils/textutils/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<a href="https://www.youtube.com/" > <h1>Youtube</h1> </a>')
+
+
+def about(request):
+    return HttpResponse('Hello About')
