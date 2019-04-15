6661dc61af6ebadac2b8fe5c0ef57d4ebf2b82cc
Manish Jayswal <jayswalmp@gmail.com>
2019-04-15 14:15:37 +0545

    second commit

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -3,6 +3,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<a href="https://www.youtube.com/" > <h1>Youtube</h1> </a>')
 
-
 def about(request):
     return HttpResponse('Hello About')
