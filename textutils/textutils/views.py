b82bba17a42196127dc90cc7330f03b0898dfb85
Manish Jayswal <jayswalmp@gmail.com>
2019-04-16 14:13:45 +0545

    Third commit

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,7 +1,60 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<a href="https://www.youtube.com/" > <h1>Youtube</h1> </a>')
+    return render(request,'index.html')
 
-def about(request):
-    return HttpResponse('Hello About')
+def analyze(request):
+    #Get the text
+    djtext = request.POST.get('text', 'default')
+
+    #check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    spaceremover = request.POST.get('spaceremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
+
+     #check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Remove Punctuations','analyzed_text':analyzed}
+        djtext = analyzed
+
+    if(fullcaps == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Changed to UpperCase', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(spaceremover == "on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] ==" " and djtext[index+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Spaces', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(newlineremover == 'on'):
+        analyzed = ""
+        for char in djtext:
+            if char!='\n'and char!='\r':
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(charcount == 'on'):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            analyzed = index + 1
+        params = {'purpose': 'Character Counted', 'analyzed_text': analyzed}
+
+    if(removepunc!='on' and newlineremover !='on' and charcount!='on' and fullcaps!='on' and spaceremover!='on' ):
+        return HttpResponse("Please select any operation and try again.!!!")
+
+    return render(request, 'analyze.html', params)
